27708bfd4b9a96e5cc7a80bbe5df216d80ccef39
Dipen <dipendraboyaju222@gmail.com>
2026-05-23 22:29:53 +0545

    empty form test

--- tests/elements/textbox.spec.js
+++ tests/elements/textbox.spec.js
@@ -2,9 +2,17 @@ import { test, expect } from "@playwright/test";
 import { TextBoxPage } from "../../pages/elements/TextBoxPage";
 import user from "../../test-data/userData.json";
 
-test("submit textbox form successfully", async ({ page }) => {
-  const textboxPage = new TextBoxPage(page);
+let textboxPage;
 
+// beforeeach hook
+test.beforeEach(async ({ page }) => {
+  textboxPage = new TextBoxPage(page);
+
+  await textboxPage.goto();
+});
+
+//submit textbox form successfully
+test("submit textbox form successfully", async ({ page }) => {
   await textboxPage.goto();
 
   await textboxPage.fillForm(user);
@@ -13,4 +21,13 @@ test("submit textbox form successfully", async ({ page }) => {
 
   await expect(textboxPage.outputName).toContainText(user.fullName);
   await expect(textboxPage.outputEmail).toContainText(user.email);
+});
+
+//submit empty form
+test("should not submit empty form", async ({ page }) => {
+  await textboxPage.goto();
+
+  await textboxPage.submitForm();
+
+  await expect(textboxPage.outputName).not.toBeVisible();
 });
