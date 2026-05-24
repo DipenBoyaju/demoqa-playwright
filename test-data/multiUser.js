8a15a5970eaab33edbfa6ff3b118713a76910286
Dipen <dipendraboyaju222@gmail.com>
2026-05-24 09:04:56 +0545

    multiple user test

---
+++ test-data/multiUser.js
@@ -0,0 +1,14 @@
+export const users = [
+  {
+    fullName: "John Doe",
+    email: "john@test.com",
+    currentAddress: "Kathmandu",
+    permanentAddress: "Nepal",
+  },
+  {
+    fullName: "Tony Stark",
+    email: "tony@test.com",
+    currentAddress: "New York",
+    permanentAddress: "USA",
+  },
+];
--- tests/elements/textbox.spec.js
+++ tests/elements/textbox.spec.js
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { TextBoxPage } from "../../pages/elements/TextBoxPage";
 import user from "../../test-data/userData.json";
+import { users } from "../../test-data/multiUser";
 
 let textboxPage;
 
@@ -21,6 +22,28 @@ test("submit textbox form successfully", async ({ page }) => {
 
   await expect(textboxPage.outputName).toContainText(user.fullName);
   await expect(textboxPage.outputEmail).toContainText(user.email);
+});
+
+//invalid email
+test.only("should show validation for invalid email", async ({ page }) => {
+  await textboxPage.fullNameInput.fill("Tony");
+
+  await textboxPage.emailInput.fill("tony123");
+
+  await textboxPage.submitForm();
+
+  await expect(textboxPage.emailInput).toHaveClass(/field-error/);
+});
+
+//submit multiple users
+users.forEach((user) => {
+  test(`submit form for ${user.fullName}`, async ({ page }) => {
+    await textboxPage.fillForm(user);
+
+    await textboxPage.submitForm();
+
+    await expect(textboxPage.outputName).toContainText(user.fullName);
+  });
 });
 
 //submit empty form
